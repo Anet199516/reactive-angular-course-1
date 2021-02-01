37f6978dc2c2898611dade9685f77e0e9d9d58ba
anna <curly160795@gmail.com>
2021-02-01 11:34:49 +0200

    master-detail UI pattern

--- src/app/search-lessons/search-lessons.component.ts
+++ src/app/search-lessons/search-lessons.component.ts
@@ -15,6 +15,7 @@ import {
 } from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import {CoursesService} from "../services/courses.service";
 
 
 @Component({
@@ -23,15 +24,25 @@ import {Lesson} from '../model/lesson';
   styleUrls: ['./search-lessons.component.css']
 })
 export class SearchLessonsComponent implements OnInit {
+  searchResults$: Observable<Lesson[]>;
+  activeLesson: Lesson;
 
-  constructor() {
+  constructor(private coursesService: CoursesService) {
+  }
 
+  onSearch(search: string) {
+    this.searchResults$ = this.coursesService.searchLessons(search);
+  }
 
+  openLesson(lesson: Lesson) {
+    this.activeLesson = lesson;
+  }
+
+  onBackToSearch() {
+    this.activeLesson = null;
   }
 
   ngOnInit() {
-
-
   }
 
 }
